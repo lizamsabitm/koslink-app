f60304606f7e4fc71e772648166746a61e1418e6
lizamsabitm <lizamsabitm@gmail.com>
2025-12-14 20:41:26 +0700

    Fix layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 1. Kita tambahkan baris ini biar kenal URL
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // 2. Kode paksa HTTPS ditaruh di sini:
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
